e5493a8a4b820a6015731327931e65fc16a61bb1
bmtrnavsky <43661875+bmtrnavsky@users.noreply.github.com>
2021-03-24 14:18:46 -0500

    Create block_initials.py
    Creating my initials in block letters using ASCII art using Python 3

---
+++ block_initials.py
@@ -0,0 +1,31 @@
+# My name is Brad Trnavsky
+# I like to fix things
+#My first Initial in code 
+B = """
+BBBB
+B   B
+B   B
+BBBB
+B   B
+B   B
+BBBB
+"""
+# My last initial in code
+T = """
+TTTTT
+  T
+  T
+  T
+  T
+  T
+  T
+  """
+initials = B + T
+print(initials)
+print("BBBB  TTTTT")
+print("B   B   T  ")
+print("B   B   T  ")
+print("BBBB    T  ")
+print("B   B   T  ")
+print("B   B   T  ")
+print("BBBB    T  ")
